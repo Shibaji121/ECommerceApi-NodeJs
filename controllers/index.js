aff00e440a0b8668e52f4c6efd57f9d821f1da23
shiba_1234 <shibajisahu121@gmail.com>
2023-05-16 00:33:34 +0530

    get all products done

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const Products = require("../models/Product");
 
+// create product
 module.exports.createProduct = async function (req, res) {
   try {
     console.log(req.body);
     const product = await Products.create(req.body.product);
     res.status(201).json({ data: { product: product } });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// get all products
+module.exports.getAllProducts = async function (req, res) {
+  try {
+    const products = await Products.find();
+    res.status(201).json({ data: { products: products } });
   } catch (error) {
     res.status(500).json(error);
   }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const productsController = require("../controllers/index");
 
 router.route("/create").post(productsController.createProduct);
+router.route("/").get(productsController.getAllProducts);
 
 module.exports = router;
